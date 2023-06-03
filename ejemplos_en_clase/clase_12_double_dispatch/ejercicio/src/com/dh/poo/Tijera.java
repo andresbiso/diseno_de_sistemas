6eec02c7925086356fcd2f95ba4009dc01df2976
Andrés Biso <biso.andres@gmail.com>
2023-06-02 22:16:27 -0300

    feat: add ejemplo_en_clase double dispatch

---
+++ ejemplos_en_clase/clase_12_double_dispatch/ejercicio/src/com/dh/poo/Tijera.java
@@ -0,0 +1,22 @@
+package com.dh.poo;
+
+public class Tijera extends Elemento {
+	public void contra(Elemento elemento) {
+		elemento.contraTijera(this);
+	}
+
+	@Override
+	public void contraPapel(Papel papel) {
+		System.out.println("Gana Tijera");
+	}
+
+	@Override
+	public void contraPiedra(Piedra piedra) {
+		System.out.println("Gana Piedra");
+	}
+
+	@Override
+	public void contraTijera(Tijera tijera) {
+		System.out.println("Empate");
+	}
+}
